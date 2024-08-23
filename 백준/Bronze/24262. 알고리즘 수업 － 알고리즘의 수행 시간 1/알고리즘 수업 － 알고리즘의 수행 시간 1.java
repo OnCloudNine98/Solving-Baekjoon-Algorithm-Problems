45a4a10b361786daa1c7b61f59a10db8a9f79f01
OnCloudNine98 <lee070789@gmail.com>
2024-08-23 15:44:33 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 96 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+        //어떤 수가 들어와도 값은 똑같기 때문에 입력받지 않아도 됨
+
+		System.out.println(1);
+		System.out.println(0);
+
+	}
+	
+}
